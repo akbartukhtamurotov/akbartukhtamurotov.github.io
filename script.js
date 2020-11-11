d7c2dc4d753eef206b2f7f4c15225980a230add7
Akbar Tukhtamurotov <akbartukhtamurotov@gmail.com>
2020-11-11 09:35:24 +0500

    Add password and roomname

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const domain = 'meet.jit.si';
 const options = {
-    roomName: 'JitsiMeetAPIExample',
+    roomName: 'Y29uc3VsdGF0aW9uMQ==',
     width: 700,
     height: 700,
     parentNode: document.querySelector('#meet')
@@ -8,7 +8,12 @@ const options = {
 const api = new JitsiMeetExternalAPI(domain, options);
 api.executeCommand('displayName', 'PatientNameGoesHere')
 api.addEventListener('participantRoleChanged', function(event) {
+    console.log("Participant role changed");
     if (event.role === "moderator") {
+        console.log("Password execute");
         api.executeCommand('password', '123qwe');
     }
+});
+api.on('passwordRequired', function() {
+    api.executeCommand('password', '123qwe')
 });
